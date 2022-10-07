7c16f4b3bd99d2ed5a16ebdc6d2bf8fac39c43c9
sryan <sryan@CIN10229L.vg.com>
2022-10-07 19:12:01 +0530

    new commit

--- sample-project/src/com/vg/training/git/samples/Sample1.java
+++ sample-project/src/com/vg/training/git/samples/Sample1.java
@@ -3,7 +3,7 @@ package com.vg.training.git.samples;
 public class Sample1 {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("hello world");
 	}
 	
 	
