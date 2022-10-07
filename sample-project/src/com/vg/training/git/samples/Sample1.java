a6684e3b9a4d744ac7928e6775ac582bef2dfc89
AntonyRoy111 <115238919+AntonyRoy111@users.noreply.github.com>
2022-10-07 19:20:15 +0530

    Revert "Merge branch 'main' of"
    This reverts commit 66310c909a0ec92814eac3d33db567e56015c7d4, reversing
    changes made to a82f9ce211c72c430542263618b8ea0bc0cda062.

--- sample-project/src/com/vg/training/git/samples/Sample1.java
+++ sample-project/src/com/vg/training/git/samples/Sample1.java
@@ -3,13 +3,7 @@ package com.vg.training.git.samples;
 public class Sample1 {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("Hello ...................  Bye");
 	}
-=======
-		System.out.println("hello");
-	}
-	
->>>>>>> branch 'main' of git@github.com:krishnanunnik/java-training-projects.git
 	
 }
